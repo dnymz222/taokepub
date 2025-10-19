1bab31a25d98279d65c4480e7d96804bec5f499b
dnymz222 <wangxuepingyl@163.com>
2025-10-19 22:42:56 +0800

    hahah

--- app/api_3_0/CommetEdit.py
+++ app/api_3_0/CommetEdit.py
@@ -13,27 +13,58 @@ import json
 import math
 
 
+# @api3.route("/comet/upload")
+# def commetpload():
+#
+#     commet = Comet(number = "C/2025 A6 (Lemmon)")
+#
+#     commet.OrbitalPeriod = str(1347.713943707547) #period 年
+#     commet.SemeMajorAxis = str(122.0093217668168) #a
+#     commet.Inclination = str(143.6632748988036)  #i
+#     commet.Ecc = str(0.9956568328248037)   #e
+#     commet.PerihelionDistance = str(0.5299068813656069)  #q
+#     commet.LongitudeAscendingNode = str(108.0976178317535) #node
+#     commet.PerihelionArgument = str(132.9672366055109) #peri
+#     commet.MeanAnomaly = str(359.9337871412166) #M
+#     commet.PerihelionPassageTime = str(2460988.037351266917) #tp
+#     commet.MeanMotion = str(0.0007313319625202579) #n
+#     commet.AphelionDistance = str(243.488736652268) #Q
+#
+#     commet.Epoch = str(2460897.5) #
+#     commet.MotionType = 3 #1.椭圆 2.抛物线 3.准抛物线
+#     commet.TotalMagnitude = str(9.8) #M1
+#     commet.TotalMagnitudeSlope = str(13.5)  # K1
+#
+#     try:
+#         db.session.add(commet)
+#         db.session.commit()
+#     except Exception as e:
+#         db.session.rollback()
+#         print(e)
+#
+#     return "done"
+
 @api3.route("/comet/upload")
 def commetpload():
 
-    commet = Comet(number = "C/2025 A6 (Lemmon)")
+    commet = Comet(number = "C/2025 R2 (SWAN)")
 
-    commet.OrbitalPeriod = str(1347.713943707547) #period 年
-    commet.SemeMajorAxis = str(122.0093217668168) #a
-    commet.Inclination = str(143.6632748988036)  #i
-    commet.Ecc = str(0.9956568328248037)   #e
-    commet.PerihelionDistance = str(0.5299068813656069)  #q
-    commet.LongitudeAscendingNode = str(108.0976178317535) #node
-    commet.PerihelionArgument = str(132.9672366055109) #peri
-    commet.MeanAnomaly = str(359.9337871412166) #M
-    commet.PerihelionPassageTime = str(2460988.037351266917) #tp
-    commet.MeanMotion = str(0.0007313319625202579) #n
-    commet.AphelionDistance = str(243.488736652268) #Q
+    commet.OrbitalPeriod = str(653.7328801265198) #period 年
+    commet.SemeMajorAxis = str(0.9933081809968088) #a
+    commet.Inclination = str(335.3481131117367)  #i
+    commet.Ecc = str(0.9933081809968088)   #e
+    commet.PerihelionDistance = str(0.5040484344935993)  #q
+    commet.LongitudeAscendingNode = str(335.3481131117367) #node
+    commet.PerihelionArgument = str(308.3147420127663) #peri
+    commet.MeanAnomaly = str(0.006287919304289602) #M
+    commet.PerihelionPassageTime = str(2460931.329433867417) #tp
+    commet.MeanMotion = str(0.001507689628792732) #n
+    commet.AphelionDistance = str(150.1421164582602) #Q
 
-    commet.Epoch = str(2460897.5) #
+    commet.Epoch = str(2460935.5) #
     commet.MotionType = 3 #1.椭圆 2.抛物线 3.准抛物线
-    commet.TotalMagnitude = str(9.8) #M1
-    commet.TotalMagnitudeSlope = str(13.5)  # K1
+    commet.TotalMagnitude = str(11.9) #M1
+    commet.TotalMagnitudeSlope = str(8.75)  # K1
 
     try:
         db.session.add(commet)
@@ -43,7 +74,6 @@ def commetpload():
         print(e)
 
     return "done"
-
 
 
 
