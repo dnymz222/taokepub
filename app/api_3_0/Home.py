5f65c8dfc457bfec0f88bfd8f45c2fea493bc982
Xueping Wang <wangxuepingyl@163.com>
2026-09-10 09:09:33 +0800

    hahahah

--- app/api_3_0/Home.py
+++ app/api_3_0/Home.py
@@ -33,7 +33,7 @@ def soluanrconfig():
 
     dict["isUpgrade"] = False
 
-    outtime = datetime.datetime(2026, 9, 10, hour=0, minute=0, second=0, microsecond=0)
+    outtime = datetime.datetime(2026, 9, 14, hour=0, minute=0, second=0, microsecond=0)
 
     nowtime = datetime.datetime.now()
 
